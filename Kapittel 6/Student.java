921e0b2919634a8735d8c735fce3374b4ab5b150
Exouxas <exouxas@gmail.com>
2022-01-27 12:48:56 +0100

    Created basic student class test

---
+++ Kapittel 6/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    //objektvariabler, attributter, eller instansvariabler
+    int studNr;
+    String fornavn;
+    String etternavn;
+
+
+
+    // CTOR
+    public Student(){
+        
+    }
+
+    public Student(int _studNr, String _fornavn, String _etternavn){
+        studNr = _studNr;
+        fornavn = _fornavn;
+        etternavn = _etternavn;
+    }
+
+    // Metoder
+}
