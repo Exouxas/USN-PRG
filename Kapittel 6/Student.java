9b9640f6cb14fe782d3ce42d60a6da86b26a0750
Exouxas <exouxas@gmail.com>
2022-02-03 11:44:52 +0100

    Created some examples from class

--- Kapittel 6/Student.java
+++ Kapittel 6/Student.java
@@ -1,21 +1,39 @@
+import static java.lang.System.*;
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+import static java.lang.Math.*;
+import java.util.*;
+
 public class Student {
     //objektvariabler, attributter, eller instansvariabler
     int studNr;
     String fornavn;
     String etternavn;
+    int fødtÅr;
+    char kjønn;
 
 
 
     // CTOR
     public Student(){
-        
+
     }
 
-    public Student(int _studNr, String _fornavn, String _etternavn){
+    public Student(int _studNr, String _fornavn, String _etternavn, int _fødtÅr, char _kjønn){
         studNr = _studNr;
         fornavn = _fornavn;
         etternavn = _etternavn;
+        fødtÅr = _fødtÅr;
+        kjønn = _kjønn;
     }
 
     // Metoder
+    public String toString(){
+        return studNr + ": " + fornavn + " " + etternavn;
+    }
+
+    public String navn(){
+        return fornavn + " " + etternavn;
+    }
 }
--- Kapittel 6/StudentTest.java
+++ Kapittel 6/StudentTest.java
@@ -10,6 +10,23 @@ public class StudentTest {
         // Opprette 1 studentobjekt
         Student s1 = new Student();
 
-        out.println("s1 = " + s1);
+        s1.studNr = 123456;
+        s1.fornavn = "ANFU";
+        s1.etternavn = "ilbyuv";
+        s1.fødtÅr = 1204;
+        s1.kjønn = 'X';
+
+
+        // Opprette 1 studentobjekt med innlest verdi
+        Student s2 = new Student();
+
+        s2.studNr = Min.lesHeltall(0, 999999, "Studentnummer: ");
+        s2.fornavn = showInputDialog("Fornavn: ");
+        s2.etternavn = showInputDialog("Fornavn: ");
+        s2.fødtÅr = Min.lesHeltall(1900, 2022 - 18, "Født: ");
+        s2.kjønn = showInputDialog("Kjønn (M/K): ").charAt(0);
+        
+        
+        
     }
 }
