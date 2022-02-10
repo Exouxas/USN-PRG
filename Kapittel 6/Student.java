8674d3e2951e3d7878659a77c6fcd1ea745043b4
Exouxas <exouxas@gmail.com>
2022-02-10 13:41:51 +0100

    Add getters and setters

--- Kapittel 6/Student.java
+++ Kapittel 6/Student.java
@@ -8,10 +8,45 @@ import java.util.*;
 public class Student {
     //objektvariabler, attributter, eller instansvariabler
     private int studNr;
-    String fornavn;
-    String etternavn;
+    public int getStudNr(){
+        return studNr;
+    }
+    public void setStudÅr(int fødtÅr){
+        this.studNr = studNr;
+    }
+
+    private String fornavn;
+    public String getFornavn(){
+        return fornavn;
+    }
+    public void setFornavn(String fornavn){
+        this.fornavn = pent(fornavn);
+    }
+
+    private String etternavn;
+    public String getEtternavn(){
+        return etternavn;
+    }
+    public void setEtternavn(String etternavn){
+        this.etternavn = pent(etternavn);
+    }
+    
     private int fødtÅr;
+    public int getFødtÅr(){
+        return fødtÅr;
+    }
+    public void setFødtÅr(int fødtÅr){
+        this.fødtÅr = fødtÅr;
+    }
+    
     private char kjønn;
+    public char getKjønn(){
+        return kjønn;
+    }
+    public void setKjønn(char kjønn){
+        this.kjønn = kjønn;
+    }
+    
 
 
 
