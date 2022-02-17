60891e8a7f12a5080b80b554a30c8f5affd16916
Exouxas <exouxas@gmail.com>
2022-02-17 10:33:36 +0100

    Create file reading template

---
+++ FilMal.java
@@ -0,0 +1,28 @@
+import static java.lang.System.*;
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+import static java.lang.Math.*;
+import java.util.*;
+
+public class Mal {
+    public static void main(String[] args) {
+        File fil = new File("tallfil.txt"); // Lag fil-peker
+        Scanner leser = new Scanner(fil); // opprett leser
+        leser.useLocale(Locale.US); // bruk punktum som separator
+
+        
+        leser.hasNext(); // bool
+        leser.next(); // String
+
+        leser.hasNextLine(); // bool
+        leser.nextLine(); // String
+
+        leser.hasNextInt(); // bool
+        leser.nextInt(); // int
+
+        leser.hasNextDouble(); // bool
+        leser.nextDouble(); // double
+
+    }
+}
