557c1fcf9da06a5d322c09a06141805b93068f49
Exouxas <exouxas@gmail.com>
2022-03-24 10:30:31 +0100

    Update template to include try-catch

--- FilMal.java
+++ FilMal.java
@@ -4,25 +4,37 @@ import static java.lang.Integer.*;
 import static java.lang.Double.*;
 import static java.lang.Math.*;
 import java.util.*;
+import java.io.*;
 
-public class Mal {
+public class FilMal {
     public static void main(String[] args) {
         File fil = new File("tallfil.txt"); // Lag fil-peker
-        Scanner leser = new Scanner(fil); // opprett leser
-        leser.useLocale(Locale.US); // bruk punktum som separator
-        // leser.useDelimitier(";"); // endre skilletegn
+        Scanner leser = null;
+        try{
+            leser = new Scanner(fil); // opprett leser
+            leser.useLocale(Locale.US); // bruk punktum som separator
+            // leser.useDelimitier(";"); // endre skilletegn
+            
+            leser.hasNext(); // bool
+            leser.next(); // String
+    
+            leser.hasNextLine(); // bool
+            leser.nextLine(); // String
+    
+            leser.hasNextInt(); // bool
+            leser.nextInt(); // int
+    
+            leser.hasNextDouble(); // bool
+            leser.nextDouble(); // double
+        }catch(Exception e){
+            out.println("Failed to read file: " + e.toString());
+        }finally{
+            if(leser != null){
+                leser.close();
+            }
+        }
+
         
-        leser.hasNext(); // bool
-        leser.next(); // String
-
-        leser.hasNextLine(); // bool
-        leser.nextLine(); // String
-
-        leser.hasNextInt(); // bool
-        leser.nextInt(); // int
-
-        leser.hasNextDouble(); // bool
-        leser.nextDouble(); // double
 
 
         
