32ce541c35592fd4657806587fb4d6d902ac4473
Exouxas <exouxas@gmail.com>
2022-07-12 16:43:09 +0200

    Create file for problem 2 v2

---
+++ AdventOfCode/Oppg2v2.java
@@ -0,0 +1,40 @@
+import static java.lang.System.*;
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+import static java.lang.Math.*;
+import java.util.*;
+import java.io.*;
+
+public class Oppg2v2 {
+    public static void main(String[] args) {
+        Scanner reader = null;
+        File adventFile = new File("Oppg2.input");
+
+        // Initialization of parameters:
+
+
+
+        // 
+        
+        try{
+            reader = new Scanner(adventFile);
+
+            while(reader.hasNextInt()){
+                
+
+
+            }
+        }catch(Exception e){
+            out.println("General failure: " + e.toString());
+        }finally{
+            if(reader != null){
+                try{
+                    reader.close();
+                }catch(Exception e){
+                    out.println("Failed to close file: " + e.toString());
+                }
+            }
+        }
+    }
+}
